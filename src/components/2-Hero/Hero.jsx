5da75407fde9c527c4d90dbf599ec2576e7ac84a
yasser elshiwy <yasser.elshiwy0@gmail.com>
2025-01-28 02:00:05 +0200

    frist commit

---
+++ src/components/2-Hero/Hero.jsx
@@ -0,0 +1,87 @@
+import profile from "../../assts/img/prtofoilophotot-modified.png";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import Lottie from "lottie-react";
+import lap from "../../../public/anmition/laptop.json";
+import { motion } from "framer-motion";
+export default function Hero() {
+  return (
+    <>
+      <section className="hero flex  items-center pt-14 px-0 md:px-16 ">
+        <div className="grow  ">
+          <div className=" relative min-h-32">
+            <motion.img
+              initial={{ transform: "scale(0)" }}
+              animate={{ transform: "scale(1)" }}
+              transition={{ damping: 6, type: "spring", stiffness: 20 }}
+              src={profile}
+              className="w-32 border-2 border-gray-300 shadow-xs shadow-gray-300 dark:border-[#fff7ed7e] rounded-full"
+              alt=""
+            />
+            <div className="text-blue-500 text-right  absolute left-[114px] bottom-1 ">
+              <VerifiedIcon />
+            </div>
+          </div>
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+            className="mt-10  mb-5 text-3xl md:text-4xl font-bold text-black dark:text-white"
+          >
+            Front-End Web Developer
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+            className="text-lg Caveat-font  tracking-wider  text-gray-600 dark:text-gray-300"
+          >
+            <p>Hi !</p>
+            I’m <span className="text-red-400">Yasser Emad Elshiwy</span> ,
+            Front-End Web Developer , transforming ideas into{" "}
+            <span className="text-red-400 ">Digital Reality</span>
+          </motion.p>
+          <div className="flex items-center gap-5 my-5 text-2xl dark:text-gray-300 ">
+            <a
+              href="https://api.whatsapp.com/send/?phone=2001275962106&text=Welcome+to+Yasser+El-shiwy%2C+front-end+developer&type=phone_number&app_absent=0"
+              target="_blank"
+              className="hover:text-green-600 hover:scale-125 transition-all duration-200"
+            >
+              <i className="fa-brands fa-whatsapp"></i>
+            </a>
+            <a
+              href="https://www.facebook.com/elshiwwy"
+              target="_blank"
+              className="hover:scale-125 hover:text-blue-600 transition-all duration-200"
+            >
+              <i className="fa-brands fa-facebook-f"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/elshiwy_x/"
+              target="_blank"
+              className="hover:scale-125 hover:text-[#FF7B00] transition-all duration-200"
+            >
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+            <a
+              href="https://github.com/yasserelshiwy"
+              target="_blank"
+              className="hover:scale-125  transition-all duration-200"
+            >
+              <i className="fa-brands fa-github"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/yasser-elshiwy-7a1415326/"
+              target="_blank"
+              className="hover:scale-125 hover:text-[#0A66C2] transition-all duration-200"
+            >
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+        <div className=" hidden xl:block">
+          <Lottie className=" h-[410px] " animationData={lap} />
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/components/3-Main/myProjects.js
@@ -0,0 +1,122 @@
+export const myProjects = [
+  {
+    titel: "Freash Care",
+    Category: "React&Tailwind",
+    image: "/img/freashcart0.png",
+    liveDemo: "https://fresh-cart-e-commers.vercel.app",
+    gitHubLink: "https://github.com/yasserelshiwy/freshCart",
+    discription:
+      "This project is an online store used in react js, tailwindcss, and it displays products. The user can create an account, browse the site, and place products in the shopping cart or in the wish list. He can buy a product by paying online or upon delivery. It displays the orders he requested and the products are divided according to their type to facilitate the user's access to a specific product and its details. He can browse within specific products according to categories.",
+    id: "1",
+  },
+  {
+    titel: "YUMMY",
+    Category: "JavaScript",
+    image: "/img/YUMMY.png",
+    liveDemo: "https://yasserelshiwy.github.io/YUMMY/",
+    gitHubLink: "https://github.com/yasserelshiwy/YUMMY",
+    discription:
+      "•This is a site that contains all the famous foods around the world and it displays them in a simple and smooth way. You can filter the foods according to the country and you can search for the food using the first letter of it or the whole word. The food details are displayed. It is built using JavaScript and Botstrap. It is considered a single-page site using DOM, so there is no loading of pages or its data, so it is easy for the user to use. ",
+    id: "2",
+  },
+  {
+    titel: "Laindwind",
+    Category: "Html&Css",
+    image: "/img/landwind.png",
+    liveDemo: "https://landwind-six.vercel.app/",
+    gitHubLink: "https://github.com/yasserelshiwy/Landwind",
+    discription:
+      "Landwind is a page for Building digital products & brands. It is made using Tailwand and supports dark mode feature which enables it to support all media and screens big and small.",
+    id: "3",
+  },
+  {
+    titel: "Bookmarker",
+    Category: "JavaScript",
+    image: "/img/Bookmarker.png",
+    liveDemo: "https://yasserelshiwy.github.io/book-market-stitic/",
+    gitHubLink: "https://github.com/yasserelshiwy/book-market-stitic",
+    discription:
+      "Bookmarker is a library that uses JavaScript and a bootstrap. You can save the books you need to read in it and you can delete them after reading them. It supports all media.",
+    id: "4",
+  },
+  {
+    titel: "Daniels",
+    Category: "Html&Css",
+    image: "/img/Daniels.png",
+    liveDemo: "https://yasserelshiwy.github.io/Daniels/",
+    gitHubLink: "https://github.com/yasserelshiwy/Daniels",
+    discription:
+      "Daniels is a landing page with bootstrap that represents the portfolio of a UI/UX Designer & Web Developer and it accepts all media.",
+    id: "9",
+  },
+  {
+    titel: "Egyption Party",
+    Category: "JavaScript",
+    image: "/img/Egyption Party.png",
+    liveDemo: "https://yasserelshiwy.github.io/Egyption-party1-/",
+    gitHubLink: "https://github.com/yasserelshiwy/Egyption-party1-",
+    discription:
+      "Egyptian Party is an invitation to a party using jQuery, bootstrap. It is a landing page that calculates the number of days remaining until the party day.",
+    id: "5",
+  },
+  {
+    titel: "Note App",
+    Category: "React&Tailwind",
+    image: "img/noteapp.png",
+    liveDemo: "https://note-app-pi-azure.vercel.app",
+    gitHubLink: "https://github.com/yasserelshiwy/Note-App",
+    discription:
+      "This project is a site for recording daily notes. The user can create his own account and save notes on it safely. The site is built entirely with react and tailwind with some libraries such as react-awesome-reveal and others. ",
+    id: "6",
+  },
+  {
+    titel: "Smart Login",
+    Category: "JavaScript",
+    image: "/img/Smart Login.png",
+    liveDemo: "https://yasserelshiwy.github.io/smart-login/",
+    gitHubLink: "https://github.com/yasserelshiwy/smart-login",
+    discription:
+      "Smart Login is an app used in JavaScript and boot strap to test the validation. The user can create an account and log in to his main page, taking into account the instructions.",
+    id: "7",
+  },
+  {
+    titel: "DevFolio",
+    Category: "Html&Css",
+    image: "/img/DevFolio.png",
+    liveDemo: "https://yasserelshiwy.github.io/devfolo/",
+    gitHubLink: "https://github.com/yasserelshiwy/devfolo",
+    discription:
+      "DevFolio is a landing page using Bootstrap that is a portfolio for full stack developer UX and web developer and accepts all media.",
+    id: "10",
+  },
+  {
+    titel: "Fokir",
+    Category: "Html&Css",
+    image: "/img/fokir.png",
+    liveDemo: "https://yasserelshiwy.github.io/Fokir/",
+    gitHubLink: "https://github.com/yasserelshiwy/Fokir",
+    discription:
+      "Fokir is a landing page using css that represents a portfolio for UI/UX Designer, UX and Web Developer and accepts all media.",
+    id: "11",
+  },
+  {
+    titel: "Fokir",
+    Category: "Html&Css",
+    image: "/img/Mealify.png",
+    liveDemo: "https://yasserelshiwy.github.io/Mealify/",
+    gitHubLink: "https://github.com/yasserelshiwy/Mealify",
+    discription:
+      "Mealify is a landing page using CSS that represents a portfolio for a restaurant that offers healthy meals. It supports darkmode using CSS only, has CSS animation, and accepts all media.",
+    id: "12",
+  },
+  {
+    titel: "Social App",
+    Category: "Next&&MUI",
+    image: "/img/Social App.png",
+    liveDemo: "https://social-app-zc9p.vercel.app",
+    gitHubLink: "https://github.com/yasserelshiwy/Social_App",
+    discription:
+      "This project is similar to a social media app and is used in NextTs and Type Script, MUI, and through it the user can create an account and log in and all the posts are shown to others and he can create a post and edit it and put a comment and edit it and he can delete them and put a personal photo and there is a page to display his personal information and it is a profile and he can change his password ",
+    id: "8",
+  },
+];
